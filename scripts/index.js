e5dfb8a4496b0714281f720299b9fe525699a0c1
Bao Nguyen <baonn2@vng.com.vn>
2023-09-18 13:55:34 +0700

    add

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,6 @@
 jQuery(document).ready(function () {
   if ("NDEFReader" in window) {
     const ndef = new NDEFReader();
-    ndef.scan().then(() => {
-        alert("Đã kết nối đến thiết bị NFC.");
-      }).catch((error) => {
-        alert("Không thể kết nối đến thiết bị NFC: " + error);
-      });
       ndef.addEventListener("reading", (event) => {
         alert("Thẻ NFC đã được đọc:", event);
         // Xử lý dữ liệu từ thẻ NFC ở đây.
